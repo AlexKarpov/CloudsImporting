bfa1433f4f05f65a52f6e0d110248a2421497597
Alexander Karpov <alex.id.live@gmail.com>
2020-02-09 17:31:35 +0200

    Update GoogleDriveProxy+GIDSign.swift

--- Source/Classes/ServiceProxies/GoogleDriveProxy+GIDSign.swift
+++ Source/Classes/ServiceProxies/GoogleDriveProxy+GIDSign.swift
@@ -2,7 +2,7 @@
 //  GoogleDriveProxy+GIDSign.swift
 //  Importing
 //
-//  Created by Stas Lavruk on 26.11.2019.
+//  Created by Alexander Karpov on 26.11.2019.
 //  Copyright © 2019 Alexander Karpov. All rights reserved.
 //
 
